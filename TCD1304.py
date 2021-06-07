8448be3e9994ea6e3ae645720251650f5c17ef50
freddiscr <82454532+freddiscr@users.noreply.github.com>
2021-06-07 16:10:04 +0200

    Non-blocking oscilloscope and redefined clocks

--- TCD1304.py
+++ TCD1304.py
@@ -1,24 +1,18 @@
-'''
+"""
 Requests for the Code:
 the code should have an power Output on the Voltagesource VDD=5V /VAD=5V
 the code should build a Masterclock with a frequency inbetween 0.8 to 4 MHz, typical 2 MHz. The Voltagerange of the Clock 
 shall be 0-4V
-The ICG is a squarewave with a high level of typical 4V over the time of 3694 Elements of the Clock.
-The SH is a squarewave with a high level of typical 4V and indicates the integrationtime. 
-The integrationtime is started with the falling flank of the signal and and lasts till the next falling flank.
+The Sensor will be run in the Shutter Mode, so the SH-Clock and the ICG-Clock must be working as following:
+The SH is a squarewave with a high level of typical 4V and indicates the integration_time, due to the oscilloscope in the triggermode the integration must be at least 1µs.
+Tha Datasheet says, that the integrationtime in shuttermode must be at least 10µs.
+The ICG is a squarewave that indicates the 'read out time' of the sample, so it has to be 3694*integration_time*samples_per_element
+The integrationtime is started with the falling flank of the SH and and lasts till the next falling flank.
 The analog input of the sensor shall be read by the the osilloscope.
-the analog voltage signal shall be saved over the time and be integrated to get a digital signal.
+The analog voltage signal shall be saved over the time and be integrated to get a digital signal.
 the algotithims still needs to be definded.
 
-import argparse
-import platform
-import os.path
-import shutil
-import sys
-import time
-from itertools import zip_longest
-from typing import List, Tuple
-'''
+"""
 
 import numpy as np
 import time
@@ -30,12 +24,14 @@ from pslab.instrument.logic_analyzer import LogicAnalyzer
 from pslab.instrument.power_supply import PowerSupply
 
 frequency_master_clock = 2e6 # no need to specify the variable type (float). frequency_master_clock = float(2e6) turns into frequency_master_clock = 2e6. Same for integration_time variable. Im Datentyp float werden reele Zahlen in Exponentialdarstellung geschrieben (also Gleitkommazahlen). float(2e6) turns 2e6 which is already a float into a float. In other words, it does nothing; However f.e. 280,7 would have also been linked to float instead of int
-integration_elements = 3694 #according to timing requests
-integration_time =  (1/frequency_master_clock)*integration_elements #1.85e-3  time in seconds
+integration_elements = 3694 # according to timing requests
+integration_time =  10e-6   #  time in seconds
 oscillator_frequency = 128e6 #frequency of the oscilliator in Mhz
 microseconds_in_second = 1e6
 min_voltage_output = 2.0 # defines the minimum voltage output of the sensor. The oscilloscope gets enabled by the trigger over that Voltage. range must be verified
 max_voltage_output = 4.0 # maximum voltage output of the sensor. must be verified
+samples_per_element = 2
+read_out_time = integration_time*integration_elements
 
 class TCD1304():
     def __init__(
@@ -62,29 +58,32 @@ class TCD1304():
         ps.pv1 = 4
         ps.pv1
     
+    def analog_signal_read (self, block:False):
+        self.scope = Oscilloscope()
+        self.scope.select_range('CH1', max_voltage_output)   # voltagerange should be fitted to the sensors output for better resolution. sensor otput is between 2V-3V due to datasheet
+        self.scope.configure_trigger(channel = 'CH1', voltage = min_voltage_output ) # starts recording , when voltage is over a certain level
+        self.scope.capture(channels=1, samples=integration_elements*samples_per_element, timegap=integration_time/samples_per_element)
+        y1, y2 = self.scope.fetch_data()
+        diff = abs(y1,y2[1, 0] - min_voltage_output )
+        analog_measurement = np.ndarray ()
+        return analog_measurement  # this should put the Measurements and timestamps in an array
+    
+    
     def master_clock (self):        # puts PWM with frequency of 2MHz on SQ1; masterclock is the fastest clock
        prescaler = int(math.log(oscillator_frequency / frequency_master_clock) / math.log(2))   # When setting a frequency by mapping the reference clock directly to a PWM output, only frequencies which are even factors of 128 MHz (the frequency of the PSLab's main oscillator) are available. The frequency is therefore not set by specifying the frequency itself, but by setting a prescaler.
-        self.pwmgen.map_reference_clock(["SQ1"], prescaler)    
+       self.pwmgen.map_reference_clock(["SQ1"], prescaler)    
     
-    def sh_clock (self):           #  on SQ2 sets pulse for the integrationtime. Running the Sensor in shutter mode try with a frequency of 1/4 of master_clock. tint (min) = 10µs
-        self.pwmgen.generate(["SQ2"], frequency_master_clock/4, [0.5] ) 
+    def sh_clock (self):           #  on SQ2 sets pulse for the integrationtime. Running the Sensor in shutter mode try with tint (min) = 10µs
+        self.pwmgen.generate(["SQ2"], 1/integration_time, [0.5] ) 
 
 
     def icg_clock (self):           #  on SQ3 starts and stops the reading of the sensor; this third clock is the slowest clock
         self.pwmgen.set_state()
-         for pulse in range(integration_elements):
+        for pulse in range(integration_elements):
             self.pwmgen.set_state(sq3=True)
-            time.sleep(integration_time / 2)
+            time.sleep(read_out_time / 2)
             self.pwmgen.set_state(sq3=False)
-            time.sleep(integration_time / 2)
+            time.sleep(read_out_time / 2)
         
     
-    def analog_signal_read (self):
-        self.scope = Oscilloscope()
-        self.scope.select_range('CH1', max_voltage_output)   # voltagerange should be fitted to the sensors output for better resolution. sensor otput is between 2V-3V due to datasheet
-       # x, y = scope.capture (1, integration_elements, (integration_time / integration_elements)*microseconds_in_second) # Parameters set are (Ch1, amount of samples(3694), time between samples (~0,5µs)) 
-        self.scope.configure_trigger(channel = 'CH1', voltage = min_voltage_output )
-        xy = scope.capture (channels = 1, samples = integration_elements, timegap = integration_time*1e3) # timegap must be redefined
-        diff = abs(xy[1, 0] - min_voltage_output )
-        analog_measurement = np.ndarray ()
-        return analog_measurement  # this should put the Measurements and timestamps in an array
+   
