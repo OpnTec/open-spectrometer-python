5b4e726b391536a03f0ef8a857d35ddf9a8f9f4c
freddiscr <82454532+freddiscr@users.noreply.github.com>
2021-06-12 10:09:22 +0200

    Updated variablenames and minor things

--- TCD1304.py
+++ TCD1304.py
@@ -1,17 +1,11 @@
 """
-Requests for the Code:
-the code should have an power Output on the Voltagesource VDD=5V /VAD=5V
-the code should build a Masterclock with a frequency inbetween 0.8 to 4 MHz, typical 2 MHz. The Voltagerange of the Clock 
-shall be 0-4V
-The Sensor will be run in the Shutter Mode, so the SH-Clock and the ICG-Clock must be working as following:
-The SH is a squarewave with a high level of typical 4V and indicates the integration_time, due to the oscilloscope in the triggermode the integration must be at least 1µs.
-Tha Datasheet says, that the integrationtime in shuttermode must be at least 10µs.
-The ICG is a squarewave that indicates the 'read out time' of the sample, so it has to be 3694*integration_time*samples_per_element
-The integrationtime is started with the falling flank of the SH and and lasts till the next falling flank.
-The analog input of the sensor shall be read by the the osilloscope.
-The analog voltage signal shall be saved over the time and be integrated to get a digital signal.
-the algotithims still needs to be definded.
-
+The Sensor shall be connected to the PSLab as Following:
+Pin 1 of the sensorboard: master_clock --> SQ1 on PSLab
+Pin 2: sh_clock --> SQ2
+Pin 3: icg_clock --> SQ3
+Pin 4: +5V --> PV1
+Pin 5: GND --> GND
+Pin 6: OS --> CH1
 """
 
 import numpy as np
@@ -23,60 +17,44 @@ from pslab.serial_handler import SerialHandler
 from pslab.instrument.logic_analyzer import LogicAnalyzer
 from pslab.instrument.power_supply import PowerSupply
 
-frequency_master_clock = 2e6 # no need to specify the variable type (float). frequency_master_clock = float(2e6) turns into frequency_master_clock = 2e6. Same for integration_time variable. Im Datentyp float werden reele Zahlen in Exponentialdarstellung geschrieben (also Gleitkommazahlen). float(2e6) turns 2e6 which is already a float into a float. In other words, it does nothing; However f.e. 280,7 would have also been linked to float instead of int
-integration_elements = 3694 # according to timing requests
-integration_time =  10e-6   #  time in seconds
-oscillator_frequency = 128e6 #frequency of the oscilliator in Mhz
-microseconds_in_second = 1e6
-min_voltage_output = 2.0 # defines the minimum voltage output of the sensor. The oscilloscope gets enabled by the trigger over that Voltage. range must be verified
-max_voltage_output = 4.0 # maximum voltage output of the sensor. must be verified
-samples_per_element = 2
-read_out_time = integration_time*integration_elements
+FREQUENCY_MASTER_CLOCK = 2e6
+INTEGRATION_ELEMENTS = 3694 
+INTEGRATION_TIME =  10e-6 
+OSCILLATOR_FREQUENCY = 128e6 
+MICROSECONDS = 1e6
+MIN_VOLTAGE_OUTPUT = 2.0
+MAX_VOLTAGE_OUTPUT = 4.0 
+SAMPLES_PER_ELEMENT = 2
+READ_OUT_TIME = INTEGRATION_TIME*INTEGRATION_ELEMENTS
 
-class TCD1304():
+class TCD1304:
     def __init__(
-        self, #Initializing; self is always first parameter of methods located inside a class and is not limited to the __init__ method but to most methods. A method is a function that belongs to a class
-        min_masterclock_frequency = 0.8e6, # no need to specify the variable type (float).  View comment in L37
-        max_masterclock_frequency = 4e6, # no need to specify the variable type (float). View comment in L37
-        frequency = frequency_master_clock, #default frequency of masterclock 2 MHz; # no need to specify the variable type (float). View comment in L37
-        min_voltagerange_clock: float = 0, #it says that min_voltagerange_clock should (!) be a float. Otherwise it would not turn into a float automatically. Output on float will be 0.0 V and on int 0 V.
-        max_voltagerange_clock: float = 4,
-        ): 
-
-        self.min_masterclock_frequency = min_masterclock_frequency #initializing starts from here (? Not sure though)
-        self.max_masterclock_frequency = max_masterclock_frequency
-        self.frequency = frequency
-        self.min_voltagerange_clock = min_voltagerange_clock
-        self.max_voltagerange_clock = max_voltagerange_clock
-        self.pwmgen = PWMGenerator() #once assigned in __init__ self.pwmgen can be used in other methods
+        self,  ): 
+        self.pwmgen = PWMGenerator()
         self.scope = Oscilloscope ()
         self.ps = PowerSupply()
         
-    def power_source ():        # puts a voltage of 4V on PV1
-        ps = PowerSupply()
-        ps.pv1 = 4
-        ps.pv1
+    def set_power_source():
+        self.ps.pv1 = 4
 
-    def icg_clock (self):           #  on SQ3 starts and stops the reading of the sensor; this third clock is the slowest clock
+    def start_icg_clock(self):
         self.pwmgen.set_state(sq3=True)
-        time.sleep(read_out_time)
+        time.sleep(READ_OUT_TIME)
         self.pwmgen.set_state(sq3=False)
 
-    def master_clock (self):        # puts PWM with frequency of 2MHz on SQ1; masterclock is the fastest clock
-       prescaler = int(math.log(oscillator_frequency / frequency_master_clock) / math.log(2))   # When setting a frequency by mapping the reference clock directly to a PWM output, only frequencies which are even factors of 128 MHz (the frequency of the PSLab's main oscillator) are available. The frequency is therefore not set by specifying the frequency itself, but by setting a prescaler.
+    def start_master_clock(self):       
+       prescaler = int(math.log(OSCILLATOR_FREQUENCY / FREQUENCY_MASTER_CLOCK) / math.log(2))  
        self.pwmgen.map_reference_clock(["SQ1"], prescaler)    
     
-    def sh_clock (self):           #  on SQ2 sets pulse for the integrationtime. Running the Sensor in shutter mode try with tint (min) = 10µs
-        self.pwmgen.generate(["SQ2"], 1/integration_time, [0.5] ) 
+    def start_sh_clock(self):         
+        self.pwmgen.generate(["SQ2"], 1/INTEGRATION_TIME, [0.5] ) 
 
-    def analog_signal_read (self, block:False):
-        self.scope.select_range('CH1', max_voltage_output)   # voltagerange should be fitted to the sensors output for better resolution. sensor otput is between 2V-3V due to datasheet
-        self.scope.configure_trigger(channel = 'CH1', voltage = min_voltage_output ) # starts recording , when voltage is over a certain level
-        self.scope.capture(channels=1, samples=integration_elements*samples_per_element, timegap=integration_time/samples_per_element, block=False)
+    def read_signal(self):
+        self.scope.select_range('CH1', MAX_VOLTAGE_OUTPUT)   
+        self.scope.configure_trigger(channel = 'CH1', voltage = MIN_VOLTAGE_OUTPUT ) 
+        self.scope.capture(channels=1, samples=INTEGRATION_ELEMENTS*SAMPLES_PER_ELEMENT, timegap=INTEGRATION_TIME/SAMPLES_PER_ELEMENT, block=False)
         self.icg_clock()
-        y, = self.scope.fetch_data()  # collecting the Voltage in nonblocking mode
-        return y; #returns samples
+        y, = self.scope.fetch_data() 
+        return y 
 
-
-    
    
